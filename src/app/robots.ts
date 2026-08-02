ef5b825fe83b1b15dd86f1e7626454073caa5035
IHARA HIROSHI <hiroshi_ihara@MTC.local>
2026-08-02 21:30:18 +0900

    Update robots.txt for uttacca.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,22 +1,14 @@
 import type { MetadataRoute } from "next";
 
-function getBaseUrl() {
-  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
-  }
-
-  return "http://localhost:3000";
-}
-
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = getBaseUrl();
-
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
 
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: "https://uttacca.com/sitemap.xml",
+
+    host: "https://uttacca.com",
   };
 }
